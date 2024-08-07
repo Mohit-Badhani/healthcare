a4469c1cd7846456273541692765a416dbc14931
Mohit-Badhani <mohitbadhani1234@gmail.com>
2024-08-07 22:12:51 +0530

    Final Update

--- app/patients/[userId]/new-appointment/page.tsx
+++ app/patients/[userId]/new-appointment/page.tsx
@@ -1,5 +1,6 @@
 import * as Sentry from "@sentry/nextjs";
 import Image from "next/image";
+
 import { AppointmentForm } from "@/components/forms/AppointmentForm";
 import { getPatient } from "@/lib/actions/patient.actions";
 
